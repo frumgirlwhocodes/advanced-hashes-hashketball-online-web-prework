763be5dad8a717e0ec1d46d0571843faeca0afeb
lieby weintraub <liebahinda@gmail.com>
2019-02-10 23:51:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,8 +2,8 @@
 def game_hash
   { 
     :home => {:team_name => "Brooklyn Nets",
-      :colors => ["black", "white"]
-      :players 
+      :colors => ["Black", "White"],
+      :players => 
     },
     
     
